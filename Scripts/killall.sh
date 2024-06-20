0f9c9176d60ab380daa6d39ccb7887f6382d15e3
Xeno Kovah <xeno@ost.fyi>
2024-06-20 18:56:26 -0400

    kill mo betta

--- Scripts/killall.sh
+++ Scripts/killall.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 #RESULT1=$(killall gpspipe)
-#echo $RESULT1
 RESULT2=$(killall btmon)
-#echo $RESULT2
 RESULT3=$(killall bluetoothctl)
-#echo $RESULT3
 RESULT4=$(killall sudo)
-#echo $RESULT4
+RESULT5=$(killall python3)
+RESULT6=$(pkill -u root bash)
