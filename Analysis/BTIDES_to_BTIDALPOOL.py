965698b1187bbec56f8de3aea9074290ff678d73
Xeno Kovah <xeno@ost.fyi>
2025-01-09 13:13:33 -0500

    moved code at the beginning for imports that was bugging me to venv_helper.py

--- Analysis/BTIDES_to_BTIDALPOOL.py
+++ Analysis/BTIDES_to_BTIDALPOOL.py
@@ -3,45 +3,11 @@
 # Copyright(c) Dark Mentor LLC 2023-2025
 ########################################
 
+# Activate venv before any other imports
 import os
 import sys
 from pathlib import Path
-def activate_venv():
-    """Activate virtual environment if it exists"""
-    script_dir = Path(__file__).parent
-    venv_path = script_dir / '../venv'
-
-    if not venv_path.exists():
-        # Because I do testing with it in a different location, try this
-        venv_path = script_dir / './venv'
-        if not venv_path.exists():
-            return False
-
-    # Get Python version from venv binary
-    venv_python = venv_path / 'bin' / 'python'
-    if not venv_python.exists():
-        return False
-
-    # Set environment variables
-    os.environ['VIRTUAL_ENV'] = str(venv_path)
-    os.environ['PATH'] = f"{venv_path}/bin:{os.environ['PATH']}"
-
-    # Remove PYTHONHOME if set
-    if 'PYTHONHOME' in os.environ:
-        del os.environ['PYTHONHOME']
-
-    # Add site-packages to path
-    for lib_dir in venv_path.glob('lib/python*/site-packages'):
-        sys.path.insert(0, str(lib_dir))
-        break
-
-    # Set base prefix
-    sys.prefix = str(venv_path)
-    sys.exec_prefix = str(venv_path)
-
-    return True
-
-# Activate venv before any other imports
+from handle_venv import activate_venv
 activate_venv()
 
 import ssl
