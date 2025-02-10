5595ecf6b43c05973cd898f41e9d2cd1840a5519
Xeno Kovah <xeno@ost.fyi>
2025-02-10 13:46:36 -0500

    Removed redundant 'opcode' term

--- Analysis/TME/TME_BTIDES_LL.py
+++ Analysis/TME/TME_BTIDES_LL.py
@@ -16,21 +16,21 @@ import TME.TME_glob
 ############################
 
 def ff_LL_CONNECTION_UPDATE_IND(direction, win_size, win_offset, interval, latency, timeout, instant):
-    obj = {"direction": direction, "opcode": type_opcode_LL_CONNECTION_UPDATE_IND, "win_size": win_size, "win_offset": win_offset, "interval": interval, "latency": latency, "timeout": timeout, "instant": instant}
+    obj = {"direction": direction, "opcode": type_LL_CONNECTION_UPDATE_IND, "win_size": win_size, "win_offset": win_offset, "interval": interval, "latency": latency, "timeout": timeout, "instant": instant}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_CONNECTION_UPDATE_IND"
     return obj
 
 
 def ff_LL_CHANNEL_MAP_IND(direction, channel_map_hex_str, instant):
-    obj = {"direction": direction, "opcode": type_opcode_LL_CHANNEL_MAP_IND, "channel_map_hex_str": channel_map_hex_str, "instant": instant}
+    obj = {"direction": direction, "opcode": type_LL_CHANNEL_MAP_IND, "channel_map_hex_str": channel_map_hex_str, "instant": instant}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_CHANNEL_MAP_IND"
     return obj
 
 
 def ff_LL_TERMINATE_IND(direction, error_code):
-    obj = {"direction": direction, "opcode": type_opcode_LL_TERMINATE_IND, "error_code": error_code}
+    obj = {"direction": direction, "opcode": type_LL_TERMINATE_IND, "error_code": error_code}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_TERMINATE_IND"
         if error_code in controller_error_strings:
@@ -39,41 +39,41 @@ def ff_LL_TERMINATE_IND(direction, error_code):
 
 
 def ff_LL_ENC_REQ(direction, rand, ediv, skd_c, iv_c):
-    obj = {"direction": direction, "opcode": type_opcode_LL_ENC_REQ, "rand": rand, "ediv": ediv, "skd_c": skd_c, "iv_c": iv_c}
+    obj = {"direction": direction, "opcode": type_LL_ENC_REQ, "rand": rand, "ediv": ediv, "skd_c": skd_c, "iv_c": iv_c}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_ENC_REQ"
     return obj
 
 
 def ff_LL_ENC_RSP(direction, skd_p, iv_p):
-    obj = {"direction": direction, "opcode": type_opcode_LL_ENC_RSP, "skd_p": skd_p, "iv_p": iv_p}
+    obj = {"direction": direction, "opcode": type_LL_ENC_RSP, "skd_p": skd_p, "iv_p": iv_p}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_ENC_RSP"
     return obj
 
 
 def ff_LL_START_ENC_REQ(direction):
-    obj = {"direction": direction, "opcode": type_opcode_LL_START_ENC_REQ}
+    obj = {"direction": direction, "opcode": type_LL_START_ENC_REQ}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_START_ENC_REQ"
     return obj
 
 
 def ff_LL_START_ENC_RSP(direction):
-    obj = {"direction": direction, "opcode": type_opcode_LL_START_ENC_RSP}
+    obj = {"direction": direction, "opcode": type_LL_START_ENC_RSP}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_START_ENC_RSP"
     return obj
 
 
 def ff_LL_VERSION_IND(direction, version, company_id, subversion):
-    obj = {"direction": direction, "opcode": type_opcode_LL_VERSION_IND, "version": version, "company_id": company_id, "subversion": subversion}
+    obj = {"direction": direction, "opcode": type_LL_VERSION_IND, "version": version, "company_id": company_id, "subversion": subversion}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_VERSION_IND"
     return obj
 
 def ff_LL_REJECT_IND(direction, error_code):
-    obj = {"direction": direction, "opcode": type_opcode_LL_REJECT_IND, "error_code": error_code}
+    obj = {"direction": direction, "opcode": type_LL_REJECT_IND, "error_code": error_code}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_REJECT_IND"
         if error_code in controller_error_strings:
@@ -81,7 +81,7 @@ def ff_LL_REJECT_IND(direction, error_code):
     return obj
 
 def ff_LL_UNKNOWN_RSP(direction, unknown_type):
-    obj = {"direction": direction, "opcode": type_opcode_LL_UNKNOWN_RSP, "unknown_type": unknown_type}
+    obj = {"direction": direction, "opcode": type_LL_UNKNOWN_RSP, "unknown_type": unknown_type}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_UNKNOWN_RSP"
     return obj
@@ -89,7 +89,7 @@ def ff_LL_UNKNOWN_RSP(direction, unknown_type):
 
 def ff_LL_FEATURE_REQ(direction, features):
     le_features_hex_str = f"{features:016x}"
-    obj = {"direction": direction, "opcode": type_opcode_LL_FEATURE_REQ, "le_features_hex_str": le_features_hex_str}
+    obj = {"direction": direction, "opcode": type_LL_FEATURE_REQ, "le_features_hex_str": le_features_hex_str}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_FEATURE_REQ"
     return obj
@@ -97,7 +97,7 @@ def ff_LL_FEATURE_REQ(direction, features):
 
 def ff_LL_FEATURE_RSP(direction, features):
     le_features_hex_str = f"{features:016x}"
-    obj = {"direction": direction, "opcode": type_opcode_LL_FEATURE_RSP, "le_features_hex_str": le_features_hex_str}
+    obj = {"direction": direction, "opcode": type_LL_FEATURE_RSP, "le_features_hex_str": le_features_hex_str}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_FEATURE_RSP"
     return obj
@@ -105,7 +105,7 @@ def ff_LL_FEATURE_RSP(direction, features):
 
 def ff_LL_PERIPHERAL_FEATURE_REQ(direction, features):
     le_features_hex_str = f"{features:016x}"
-    obj = {"direction": direction, "opcode": type_opcode_LL_PERIPHERAL_FEATURE_REQ, "le_features_hex_str": le_features_hex_str}
+    obj = {"direction": direction, "opcode": type_LL_PERIPHERAL_FEATURE_REQ, "le_features_hex_str": le_features_hex_str}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PERIPHERAL_FEATURE_REQ"
     return obj
@@ -114,7 +114,7 @@ def ff_LL_PERIPHERAL_FEATURE_REQ(direction, features):
 def ff_LL_CONNECTION_PARAM_REQ(direction, interval_min, interval_max, latency, timeout, preferred_periodicity, reference_conneventcount, offset0, offset1, offset2, offset3, offset4, offset5):
     obj = {
         "direction": direction,
-        "opcode": type_opcode_LL_CONNECTION_PARAM_REQ,
+        "opcode": type_LL_CONNECTION_PARAM_REQ,
         "interval_min": interval_min,
         "interval_max": interval_max,
         "latency": latency,
@@ -136,7 +136,7 @@ def ff_LL_CONNECTION_PARAM_REQ(direction, interval_min, interval_max, latency, t
 def ff_LL_CONNECTION_PARAM_RSP(direction, interval_min, interval_max, latency, timeout, preferred_periodicity, reference_conneventcount, offset0, offset1, offset2, offset3, offset4, offset5):
     obj = {
         "direction": direction,
-        "opcode": type_opcode_LL_CONNECTION_PARAM_RSP,
+        "opcode": type_LL_CONNECTION_PARAM_RSP,
         "interval_min": interval_min,
         "interval_max": interval_max,
         "latency": latency,
@@ -156,7 +156,7 @@ def ff_LL_CONNECTION_PARAM_RSP(direction, interval_min, interval_max, latency, t
 
 
 def ff_LL_REJECT_EXT_IND(direction, reject_opcode, error_code):
-    obj = {"direction": direction, "opcode": type_opcode_LL_REJECT_EXT_IND, "reject_opcode": reject_opcode, "error_code": error_code}
+    obj = {"direction": direction, "opcode": type_LL_REJECT_EXT_IND, "reject_opcode": reject_opcode, "error_code": error_code}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_REJECT_EXT_IND"
         if error_code in controller_error_strings:
@@ -165,56 +165,56 @@ def ff_LL_REJECT_EXT_IND(direction, reject_opcode, error_code):
 
 
 def ff_LL_PING_REQ(direction):
-    obj = {"direction": direction, "opcode": type_opcode_LL_PING_REQ}
+    obj = {"direction": direction, "opcode": type_LL_PING_REQ}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PING_REQ"
     return obj
 
 
 def ff_LL_PING_RSP(direction):
-    obj = {"direction": direction, "opcode": type_opcode_LL_PING_RSP}
+    obj = {"direction": direction, "opcode": type_LL_PING_RSP}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PING_RSP"
     return obj
 
 
 def ff_LL_LENGTH_REQ(direction, max_rx_octets, max_rx_time, max_tx_octets, max_tx_time):
-    obj = {"direction": direction, "opcode": type_opcode_LL_LENGTH_REQ, "max_rx_octets": max_rx_octets, "max_rx_time": max_rx_time, "max_tx_octets": max_tx_octets, "max_tx_time": max_tx_time}
+    obj = {"direction": direction, "opcode": type_LL_LENGTH_REQ, "max_rx_octets": max_rx_octets, "max_rx_time": max_rx_time, "max_tx_octets": max_tx_octets, "max_tx_time": max_tx_time}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_LENGTH_REQ"
     return obj
 
 
 def ff_LL_LENGTH_RSP(direction, max_rx_octets, max_rx_time, max_tx_octets, max_tx_time):
-    obj = {"direction": direction, "opcode": type_opcode_LL_LENGTH_RSP, "max_rx_octets": max_rx_octets, "max_rx_time": max_rx_time, "max_tx_octets": max_tx_octets, "max_tx_time": max_tx_time}
+    obj = {"direction": direction, "opcode": type_LL_LENGTH_RSP, "max_rx_octets": max_rx_octets, "max_rx_time": max_rx_time, "max_tx_octets": max_tx_octets, "max_tx_time": max_tx_time}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_LENGTH_RSP"
     return obj
 
 
 def ff_LL_PHY_REQ(direction, tx_phys, rx_phys):
-    obj = {"direction": direction, "opcode": type_opcode_LL_PHY_REQ, "TX_PHYS": tx_phys, "RX_PHYS": rx_phys}
+    obj = {"direction": direction, "opcode": type_LL_PHY_REQ, "TX_PHYS": tx_phys, "RX_PHYS": rx_phys}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PHY_REQ"
     return obj
 
 
 def ff_LL_PHY_RSP(direction, tx_phys, rx_phys):
-    obj = {"direction": direction, "opcode": type_opcode_LL_PHY_RSP, "TX_PHYS": tx_phys, "RX_PHYS": rx_phys}
+    obj = {"direction": direction, "opcode": type_LL_PHY_RSP, "TX_PHYS": tx_phys, "RX_PHYS": rx_phys}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PHY_RSP"
     return obj
 
 
 def ff_LL_PHY_UPDATE_IND(direction, phy_c_to_p, phy_p_to_c, instant):
-    obj = {"direction": direction, "opcode": type_opcode_LL_PHY_UPDATE_IND, "phy_c_to_p": phy_c_to_p, "phy_p_to_c": phy_p_to_c, "instant": instant}
+    obj = {"direction": direction, "opcode": type_LL_PHY_UPDATE_IND, "phy_c_to_p": phy_c_to_p, "phy_p_to_c": phy_p_to_c, "instant": instant}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_PHY_UPDATE_IND"
     return obj
 
 
 def ff_LL_UNKNOWN_CUSTOM(direction, full_pkt_hex_str):
-    obj = {"direction": direction, "opcode": type_opcode_LL_UNKNOWN_CUSTOM, "full_pkt_hex_str": full_pkt_hex_str}
+    obj = {"direction": direction, "opcode": type_LL_UNKNOWN_CUSTOM, "full_pkt_hex_str": full_pkt_hex_str}
     if(TME.TME_glob.verbose_BTIDES):
         obj["opcode_str"] = "LL_UNKNOWN_CUSTOM"
     return obj
